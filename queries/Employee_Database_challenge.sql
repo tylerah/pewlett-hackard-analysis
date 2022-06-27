b707b59019b554c5c22df14bf7324577874b3218
Tyler Hendricks <tylerhendricks@mail.weber.edu>
2022-06-26 20:14:42 -0600

    added deliverable #2

--- queries/Employee_Database_challenge.sql
+++ queries/Employee_Database_challenge.sql
@@ -32,3 +32,20 @@ ORDER BY COUNT (ut.emp_no) DESC;
 
 
 -- Deliverable #2
+-- create mentorship_eligiblity table
+SELECT DISTINCT ON (e.emp_no) e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mentorship_eligibility
+FROM employees as e
+	INNER JOIN dept_emp as de
+	ON (e.emp_no = de.emp_no)
+	INNER JOIN titles as t
+	ON (e.emp_no = t.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+	AND (de.to_date = '9999-01-01')
+ORDER BY e.emp_no;
